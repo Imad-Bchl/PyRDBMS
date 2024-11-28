21ed6038ff946c166c8739894e1104f736f59d76
Ramzy <115043043+DZ-Ramzy@users.noreply.github.com>
2024-11-28 11:37:51 +0100

    Update BufferManager.py

--- BufferManager.py
+++ BufferManager.py
@@ -1,29 +1,30 @@
 import json
 
+
 class BufferManager:
     def __init__(self, db_config, disk_manager):
         self.db_config = db_config
         self.disk_manager = disk_manager
         self.buffer_pool = []
 
-    def GetPage(self, pageId):
+    def getPage(self, pageId):
         for i, (pid, buffer) in enumerate(self.buffer_pool):
             if pid == pageId:
                 self.buffer_pool.append(self.buffer_pool.pop(i))
                 return buffer
-    #If the page is not in the buffer pool 
-    # the buffer manager reads the page from the disk manager and adds it to the buffer pool.
-    
+        # If the page is not in the buffer pool
+        # the buffer manager reads the page from the disk manager and adds it to the buffer pool.
+
         buffer = bytearray(self.db_config.pageSize)
         self.disk_manager.ReadPage(pageId, buffer)
-        #ToDo : Implement the buffer replacement policy and buffer capacity 
+        # ToDo : Implement the buffer replacement policy and buffer capacity
         if len(self.buffer_pool) >= self.buffer_capacity:
             self.buffer_pool.pop(0)
         self.buffer_pool.append((pageId, buffer))
 
         return buffer
 
-    def FreePage(self, pageId, valdirty):
+    def freePage(self, pageId, valdirty):
         for i, (pid, buffer, pin_count, dirty_flag) in enumerate(self.buffer_pool):
             if pid == pageId:
                 if pin_count > 0:
@@ -35,5 +36,4 @@ class BufferManager:
                     dirty_flag = True
 
                 self.buffer_pool[i] = (pid, buffer, pin_count, dirty_flag)
-                return 
-
+                return
